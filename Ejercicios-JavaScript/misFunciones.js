a170dc0701f16860c4b880591629e571755cc7af
RamiroGhilino <ghilinoramiro@gmail.com>
2020-06-20 16:23:04 -0300

    Conversor de Unidades completo

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,5 +1,5 @@
 /**
- * Created by Agus on 6/5/2017.
+
  */
 
 /**
@@ -9,3 +9,28 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function cambiarunidades(Id, valor){
+    if(isNaN(valor)){
+        alert("Se ingresó un valor inválido");
+        document.LasUnidades.unid_metro.value="";
+        document.LasUnidades.unid_pulgada.value="";
+        document.LasUnidades.unid_pie.value="";
+        document.LasUnidades.unid_yarda.value="";
+    }else if(Id=="metro"){
+        document.LasUnidades.unid_pulgada.value=39.3701*valor;
+        document.LasUnidades.unid_pie.value=3.28084*valor;
+        document.LasUnidades.unid_yarda.value=1.09361*valor;
+    }else if(Id=="pulgada"){
+        document.LasUnidades.unid_metro.value=0.0254*valor;
+        document.LasUnidades.unid_pie.value=0.0833333*valor;
+        document.LasUnidades.unid_yarda.value=0.0277778*valor;
+    }else if(Id=="pie"){
+        document.LasUnidades.unid_metro.value=0.3047996952*valor;
+        document.LasUnidades.unid_pulgada.value=11.999988*valor;
+        document.LasUnidades.unid_yarda.value=0.333333*valor;
+    }else if(Id=="yarda"){
+        document.LasUnidades.unid_metro.value=0.9144*valor;
+        document.LasUnidades.unid_pulgada.value=36*valor;
+        document.LasUnidades.unid_pie.value=3*valor;
+    }
+}
