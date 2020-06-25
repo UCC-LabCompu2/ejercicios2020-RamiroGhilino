83fd16538da0089ede6f45e9a4bb2aa68d044eff
RamiroGhilino <ghilinoramiro@gmail.com>
2020-06-25 19:32:09 -0300

    Dibujar imagen

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -198,3 +198,17 @@ function Dibujar() {
     ctx.stroke();
     ctx.closePath();
 }
+
+function DibujarImagen(posX, posY) {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    console.log(posX,posY);
+    var img= new Image();
+    img.src= "images/auto.png";
+
+    canvas.width=canvas.width;
+    img.onload = function(){
+        ctx.drawImage(img, posX, posY);
+    };
+}
